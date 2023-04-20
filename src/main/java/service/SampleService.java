1ebd07c6222982247efc9db0ee4bcbc1a7a2a725
sato064 <satoberu@gmail.com>
2023-04-20 15:51:30 +0900

    add source

---
+++ src/main/java/service/SampleService.java
@@ -0,0 +1,30 @@
+package service;
+
+import dao.SampleDao;
+import model.Sample;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class SampleService {
+
+    private Connection connection = null;
+
+    private void createConnection(SampleDao dao) {
+        this.connection = dao.createConnection();
+    }
+
+    private void closeConnection(SampleDao dao) {
+        dao.closeConnection(this.connection);
+        this.connection = null;
+    }
+
+    public List<Sample> getSample() {
+        SampleDao dao = new SampleDao();
+        createConnection(dao);
+        List<Sample> sample = dao.findAll(connection);
+        closeConnection(dao);
+        return sample;
+    }
+
+}
